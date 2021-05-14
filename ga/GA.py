0c60fca9ae2cd12ebaf7371b10e5248f81d95e8f
hanyiik <yiixhanyiik@gmail.com>
2021-05-14 19:27:33 +0800

    more explainable.

--- ga/GA.py
+++ ga/GA.py
@@ -13,11 +13,11 @@ class Life(object):
         """
         :: 功能: 初始化一个种群中的个体
         :: 输入: aGene - 一个个体的基因，如 TSP 问题的基因就是随机排列整数的 list - e.g. [0, 22, 23, 7, 1, 21, 13, 29, 28, 16, 18, 12, 15, 9, 2, 20, 30, 17, 31, 24, 26, 27, 19, 3, 5, 4, 10, 8, 25, 32, 33, 6, 11, 14]
-        :: 输出: 一个含有基因序列与初始化分数为 -1 的个体
+        :: 输出: 一个含有[基因序列]与初始化[分数]为 -1 的个体
         :: 用法: life = Life(aGene = xxx)
         """
         self.gene = aGene
-        self.score = -1
+        self.score = 0
 
 
 class GA(object):
@@ -33,7 +33,7 @@ class GA(object):
         self.generation = 1                     # 第几代
         self.crossCount = 0                     # 统计: 交叉了几次？
         self.mutationCount = 0                  # 统计: 变异了几次？
-        self.bounds = 0.0                       # 适配值之和，用于选择是计算概率
+        self.bounds = 0.0                       # 种群中所有个体的适配值 score 之和，用于计算选择概率
 
         self.initPopulation()                   # 随机初始化 self.lives 里的 Life
 
@@ -54,24 +54,45 @@ class GA(object):
 
     def judge(self):
         """
-        :: 功能: 评估，计算每一个个体的适配值
-        :: 输入: self -
-        :: 输出:
-        :: 用法:
+        :: 功能: 计算种群中每一个个体的适配值 score, 并找出 score 最高的最优个体。
+        :: 输入: NULL
+        :: 输出: 更新种群 self.lives 中所有 Life 类型个体的 score 值;
+                更新种群 self.lives 中所有 Life 类型个体 score 之和 ——— self.bounds 的值;
+                找到种群 self.lives 中的"最优个体"，并赋值给 self.best。
+        :: 用法: self.judge()
         """
-        self.bounds = 0.0
-        self.best = self.lives[0]
+        self.bounds = 0.0           # 种群中所有个体的适配值 score 之和
+        self.best = self.lives[0]   # 最优个体
+
         for life in self.lives:
+            # 得到最优个体
             life.score = self.matchFun(life)
-            self.bounds += life.score
             if self.best.score < life.score:
                 self.best = life
 
+            # 计算种群所有个体的 score 之和
+            self.bounds += life.score
+
+
+    def getOne(self):
+        """
+        :: 功能: 选择一个个体
+        :: 输入: NULL
+        :: 输出:
+        :: 用法:
+        """
+        r = random.uniform(0, self.bounds)
+        for life in self.lives:
+            r -= life.score
+            if r <= 0:
+                return life
+
+        raise Exception("选择错误", self.bounds)
 
     def cross(self, parent1, parent2):
         """
         :: 功能: 交叉
-        :: 输入: self, parent1, parent2 -
+        :: 输入: parent1, parent2 -
         :: 输出:
         :: 用法:
         """
@@ -90,11 +111,10 @@ class GA(object):
         self.crossCount += 1
         return newGene
 
-
     def mutation(self, gene):
         """
         :: 功能: 变异
-        :: 输入: self, gene -
+        :: 输入: gene -
         :: 输出:
         :: 用法:
         """
@@ -106,37 +126,20 @@ class GA(object):
         self.mutationCount += 1
         return newGene
 
-
-    def getOne(self):
-        """
-        :: 功能: 选择一个个体
-        :: 输入: self -
-        :: 输出:
-        :: 用法:
-        """
-        r = random.uniform(0, self.bounds)
-        for life in self.lives:
-            r -= life.score
-            if r <= 0:
-                return life
-
-        raise Exception("选择错误", self.bounds)
-
-
     def newChild(self):
         """
         :: 功能: 产生新后代
-        :: 输入: self -
-        :: 输出:
+        :: 输入: NULL
+        :: 输出: Life 类型的后代个体
         :: 用法:
         """
-        parent1 = self.getOne()
+        parent1 = self.getOne()         # 选择家长 1
         rate = random.random()
 
         # 按概率交叉
         if rate < self.crossRate:
             # 交叉
-            parent2 = self.getOne()
+            parent2 = self.getOne()     # 选择家长 2
             gene = self.cross(parent1, parent2)
         else:
             gene = parent1.gene
@@ -152,13 +155,17 @@ class GA(object):
     def next(self):
         """
         :: 功能: 产生下一代
-        :: 输入: self -
-        :: 输出:
-        :: 用法:
+        :: 输入: NULL
+        :: 输出: 保留上一代的最优个体;
+                产生若干新生代个体，要够 lifeCount-1 个;
+                更新种群 self.lives 为新生代;
+                设置 self.generation + 1。
+        :: 用法: GA.next()
         """
         self.judge()
         newLives = [self.best]
-        while len(newLives) < self.lifeCount:
+        while len(newLives) < self.lifeCount:       # 直到 newLives 中的个体数 == 设定的 lifeCount (200)
             newLives.append(self.newChild())
+
         self.lives = newLives
         self.generation += 1
